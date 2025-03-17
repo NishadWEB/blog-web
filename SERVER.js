73d250e72df1ef3cb72847e043d5a60493771ea6
Nishad M <04nishad@gmail.com>
2025-03-17 14:56:00 +0530

    Initial Commit

---
+++ SERVER.js
@@ -0,0 +1,171 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+let app = express();
+let port = process.env.port || 3000;
+
+var counter = 0;
+
+var monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+var D = new Date();
+
+var Yr = D.getFullYear();
+var Mnth = monthNames[D.getMonth()];
+var Dte = D.getDate();
+var Tme = D.toLocaleTimeString();
+
+var blogs = [];
+var copyOfBlogsArray = [...blogs];
+
+var monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true })); // heyy .. this is to parse FORM data to js object
+app.use(express.json()); // and this is to parse JSON to js object ( for eidt functionailty where i sent json data throught fetch api in event listener...)
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log(blogs);
+  res.render("home.ejs", {
+    blog: blogs,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/public/html_css_js/about.html");
+});
+
+app.get("/create", (req, res) => {
+  res.sendFile(__dirname + "/public/html_css_js/create-blog.html");
+});
+
+app.get("/blog", (req, res) => {
+  res.send(blogs);
+});
+
+//CREATE BLOG
+app.post("/submit", (req, res) => {
+  var Title = req.body.formTitle; //name = title
+  var Quote = req.body.formQuote; //name = quote
+
+  var d = new Date();
+
+  var year = d.getFullYear();
+  var month = monthNames[d.getMonth()];
+  var date = d.getDate();
+  var time = d.toLocaleTimeString();
+
+  console.log();
+  console.log("before SUBMITTING the FORM : " + JSON.stringify(blogs));
+  console.log();
+
+  let formData = {
+    id: counter++,
+    title: Title,
+    quote: Quote,
+    date: `${year} ${month} ${date} , ${time}`
+  };
+
+  blogs.push(formData);
+
+  console.log();
+  console.log("after SUBMITTING the FORM : " + JSON.stringify(blogs));
+  console.log('...');
+
+  res.redirect("/create"); //  heyyy yuuu....remember , URL act as a GET request
+});
+
+// DELETE
+app.post("/delete", (req, res) => {
+  console.log('recieved in /delete server ');
+  var id = parseInt(req.body.id); // name = id (in number)
+  // blogs = blogs.filter((blog) => blog.id !== id); //filters out the false condition
+  
+  var actualIndex = blogs.findIndex(element => element.id === id);
+
+  delete blogs[actualIndex];
+  console.log('deleted blog post is : ' + JSON.stringify(blogs[actualIndex]));``
+
+  blogs = blogs.filter(blog => blog !== undefined)
+  console.log('blogs after filterng form the undefined values in the blogs array !' + JSON.stringify (blogs));
+
+    res.send({
+      blog : blogs
+    })
+  
+});
+
+//EDIT
+app.post("/edit", (req, res) => {
+
+  var ID = parseInt(req.body.id); // use this.id for extracting the actual id value form the blog in home page..
+  var newTitle = req.body.title;
+  var newQuote = req.body.quote;
+
+  var d = new Date();
+
+  var year = d.getFullYear();
+  var month = monthNames[d.getMonth()];
+  var date = d.getDate();
+  var time = d.toLocaleTimeString();
+
+  console.log('...');
+  console.log("before edting : " + JSON.stringify(blogs));
+  console.log('...');
+
+  //returns the object with obj.id === ID which is extracted fromt the req body
+  var blogToEdit = blogs.find(element => element.id === ID);
+  console.log('blog to edit is : '+ JSON.stringify(blogToEdit));
+
+  blogToEdit.title = newTitle;
+  blogToEdit.quote = newQuote;
+  blogToEdit.date = `${year} ${month} ${date} , ${time}`;
+
+  console.log('...');
+  console.log("After edting : " + JSON.stringify(blogs));
+  console.log('...');
+
+  var data = {
+    title : blogToEdit.title,
+    quote : blogToEdit.quote,
+    date : blogToEdit.date
+  };
+
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log();
+  console.log(`listening on port ${port}`);
+});
---
+++ public/html_css_js/script.js
@@ -0,0 +1,109 @@
+if(window.innerWidth < 768){
+  document.body.innerHTML = '<h1 class="errorMsg">Not available on phones</h1>'
+}
+
+//form title
+$("#title").keydown((event) => {
+  if (event.key === "Enter") {
+    emptySpaceChecker(event);
+  }
+});
+
+//form quote
+$("#quote").keydown((event) => {
+  if (event.key === "Enter" && event.shiftKey) {
+    console.log("shift + enter is pressed");
+  } else if (event.key === "Enter") {
+    event.preventDefault(); // next line prevention
+    emptySpaceChecker(event);
+  }
+});
+
+//form submit button
+$(".subBtn").click((event) => {
+  emptySpaceChecker(event);
+});
+
+//edit functionaity
+$(".editBtn").click(function (event) {
+  var Title = prompt("enter the TITLE to edit");
+  var Quote = prompt("enter the QUOTE to edit");
+
+  if (Title === "" || Quote === "") {
+    alert("Warning : fill both TITLE and QUOTE !");
+    return;
+  } else if (Title === null || Quote === null) {
+    //if canceled the prompt then the value will be NULL so this condition
+    return;
+  }
+
+  var idOfPost = parseInt($(this).parent().parent().attr("id"));
+
+  var data = {
+    id: idOfPost,
+    title: Title,
+    quote: Quote,
+  };
+
+  //this is to POST the edited data
+  fetch("http://localhost:3000/edit", {
+    method: "post",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((updatedPost) => {
+      //Dynamic updation sucess hehe ...
+      var newDate = updatedPost.date;
+      var currentElement = $(this).parent().parent(); // selecting the div .blog
+
+      $(currentElement).children().eq(0).text(Title); // title h3
+      $(currentElement).children().eq(1).text(Quote); //qiote p
+      $(currentElement).children().eq(2).text(newDate); //date p.time
+    })
+    .catch((error) => console.log("error caught : " + error));
+});
+
+$(".delBtn").click(function () {
+  var postToDelete = $(this).parent().parent();
+  var idOfPost = parseInt(postToDelete.attr("id"));
+
+  postToDelete.children().fadeOut(1000);
+  postToDelete.slideUp(1700);
+
+  var data = { id: idOfPost };
+
+  //GET reqst to update the blogs[] array in the server side
+  fetch("http://127.0.0.1:3000/delete", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+  .then(res => res.json())
+  .then(data => {
+    if(data.blog.length === 0){ //recieved data is =>  blog : arrayOfBlogs (i.e = blogs)
+      setTimeout(() => location.reload() , 1700)
+      
+    }
+  })
+  .catch(() => console.log('error cuaght!'));
+ 
+  
+  // postToDelete.remove();
+});
+
+
+
+function emptySpaceChecker(event) {
+  var quote = $("#quote").val().trim();
+  var title = $("#title").val().trim();
+
+  if (title === "" || quote === "") {
+    alert("Fill the both input fields !");
+    event.preventDefault();
+    // heyy uu... u can also use preventDefault() , this prevents the default action , that is ,in our case submitting the form as default behaviour of the inout field ,,, hehe..
+    // return false ,. prevents both , default action and the event bubbling up , i mean propogation
+  } else {
+    $("#blogForm").submit();
+  }
+}
